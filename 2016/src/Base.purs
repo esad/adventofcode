b33a220b6b1ba15b8386bd29fe44439fa02215df
Esad Hajdarevic <esad@esse.at>
2016-12-09 00:13:58 +0100

    Day5 part 2

--- 2016/src/Base.purs
+++ 2016/src/Base.purs
@@ -1,8 +1,9 @@
-module Base (module Prelude, (|>), module Control.Monad.Eff, module Control.Monad.Eff.Console) where
+module Base (module Prelude, (|>), module Control.Monad.Eff, module Control.Monad.Eff.Console, module Data.Maybe) where
 
 import Prelude
 import Data.Function (applyFlipped)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Maybe
 
 infixl 1 applyFlipped as |>
--- 2016/src/Day5.purs
+++ 2016/src/Day5.purs
@@ -1,12 +1,12 @@
 module Day5 where
 
-import Debug.Trace
 import Base
 import Data.String as String
+import Data.Char as Char
 import Data.List as List
 import Data.List ((:), List(..))
 import Data.Array as Array
-import Helpers
+import Helpers (md5)
 
 findPassword :: String -> String
 findPassword doorId =
@@ -24,6 +24,27 @@ findPassword doorId =
         _ ->
           aux (k+1) cs
 
+findFancyPassword :: String -> String
+findFancyPassword doorId =
+  aux 0 (Array.replicate 8 '.')
+  where
+    aux :: Int -> Array Char -> String
+    aux k cs =
+      case doorId <> show k |> md5 |> String.toCharArray |> List.fromFoldable of
+        '0':'0':'0':'0':'0':i:c:_ ->
+          let
+            idx = (Char.toCharCode i) - (Char.toCharCode '0')
+            result = fromMaybe cs $ Array.modifyAt idx (\x -> if x == '.' then c else x) cs
+          in
+          if Array.elemIndex '.' result == Nothing then
+            String.fromCharArray result
+          else
+            aux (k+1) result
+        _ ->
+          aux (k+1) cs
+
 main :: forall eff. Eff (console :: CONSOLE | eff) Unit
 main = do
   log $ show $ findPassword "wtnhxymk"
+  log $ show $ findFancyPassword "wtnhxymk"
+  -- log $ show $ findFancyPassword "abc"
