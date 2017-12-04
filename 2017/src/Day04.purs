00d63c3dbe3a0dbf53214e3f4fe8ab8999f10895
Esad Hajdarevic <esad@esse.at>
2017-12-04 10:58:17 +0100

    Day 4 part 2

--- 2017/src/Day04.purs
+++ 2017/src/Day04.purs
@@ -1,10 +1,10 @@
 module Day04 where
 
-import Data.String (split, Pattern(..), null)
+import Data.String (split, Pattern(..), null, joinWith, fromCharArray, toCharArray)
 import Data.Set (member, insert, empty)
 import Data.List (List(..), (:), fromFoldable)
 import Data.Foldable (foldl)
-import Data.Array (filter)
+import Data.Array (filter, sort)
 import Base
 
 
@@ -19,12 +19,22 @@ solve1 :: String -> Int
 solve1 =
   split (Pattern "\n") >>> filter (not null) >>> foldl (\acc x -> if valid x then acc+1 else acc) 0
 
+solve2 :: String -> Int
+solve2 =
+  split (Pattern "\n") >>> filter (not null) >>> map sortWords >>> joinWith "\n" >>> solve1
+  where
+    sortWords = split (Pattern " ") >>> map (toCharArray >>> sort >>> fromCharArray) >>> joinWith " "
+
 main = runTest do
   suite "Day4" do
     test "part 1" do
       equal 1 $ solve1 "aa bb cc dd ee"
       equal 0 $ solve1 "aa bb cc dd aa\naa bb cc dd aa"
       equal 1 $ solve1 "aa bb cc dd aaa"
+    test "part 2" do
+      equal 0 $ solve2 "oiii ioii iioi iiio\nabcde xyz ecdab\n"
+      equal 3 $ solve2 "abcde fghij\na ab abc abd abf abj\niiii oiii ooii oooi oooo\n"
     test "input" do
       input <- readFile "./inputs/Day04.txt"
       log $ show $ solve1 input
+      log $ show $ solve2 input
