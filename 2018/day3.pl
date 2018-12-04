ca96a6d2264d8c8130ab6d94a14c2327cf8857e9
Esad Hajdarevic <esad@esse.at>
2018-12-04 18:40:13 +0100

    Part 2

--- 2018/day3.pl
+++ 2018/day3.pl
@@ -22,10 +22,18 @@ paint(R, Tree, Tree2) :-
 
 count(_-V, C, C2) :- V >= 1 -> C2 is C + 1 ; C2 is C.
 
+coll(r(_, X1, Y1, W1, H1), r(_, X2, Y2, W2, H2)) :- side_collision(X1-W1, X2-W2), side_collision(Y1-H1, Y2-H2).
+
+side_collision(A1-A2, B1-B2) :- A1 > B1, side_collision(B1-B2, A1-A2).
+side_collision(A1-A2, B1-_) :- A1 =< B1, A2P is A1+A2-1, between(A1, A2P, B1).
+
+exclusive(Rs, E) :- member(E, Rs), foreach((member(R, Rs), R \= E), \+ coll(E, R)).
+
 main :-
   input(Lines),
   maplist(parse, Lines, Rs),
   rb_empty(T),
   foldl(paint, Rs, T, T2),
   rb_fold(count, T2, 0, Total),
-  writeln(Total).
+  exclusive(Rs, E),
+  writeln(Total), writeln(E).
