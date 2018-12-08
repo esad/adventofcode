6ccd4d29222f7b64a0ebedf4b1ba3c77f81a55e9
Esad Hajdarevic <esad@esse.at>
2018-12-08 11:20:17 +0100

    Day 8 part 2

--- 2018/day8.pl
+++ 2018/day8.pl
@@ -16,7 +16,19 @@ meta(X, [M | Ms]) --> integer(M), {XX is X - 1}, ` `, meta(XX, Ms).
 sum_metadata(node(Children, Meta), S) :-
   maplist(sum_metadata, Children, CS), sum_list(CS, S1), sum_list(Meta, S2), S is S1 + S2.
 
+get_children_ref(_, [], []).
+get_children_ref(Children, [M | Meta], Result) :-
+  get_children_ref(Children, Meta, R),
+  (nth1(M, Children, C) -> Result = [C | R] ; Result = R).
+
+value(node([], Meta), V) :- sum_list(Meta, V).
+value(node(Children, Meta), V) :- 
+  get_children_ref(Children, Meta, Refs),
+  maplist(value, Refs, S), sum_list(S, V).
+
 main :-
   read_string(user_input, _, S), string_codes(S, C), phrase(node(N), C, _),
   sum_metadata(N, Part1),
-  writeln(Part1).
+  writeln(Part1),
+  value(N, Part2),
+  writeln(Part2).
